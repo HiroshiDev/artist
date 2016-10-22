a84d27bb5698619766c67f45106b102518d2b38c
HiroshiDev <hirosi.dev@gmail.com>
2016-10-23 00:48:55 +0900

    first

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="日本画家、因幡都頼の公式WEBサイトです。展覧会のお知らせや、過去の作品・展示などがご覧いただけます。">
+	<meta name="keywords" content="因幡都頼,いなばとらい,inabatorai,日本画,武蔵野美術大学,むさび">
+	<meta property="og:url" content="http://inabatorai.com/" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="日本画家 因幡都頼 公式WEBサイト" />
+	<meta property="og:description" content="日本画家、因幡都頼の公式WEBサイトです。展覧会のお知らせや、過去の作品・展示などがご覧いただけます。" />
+	<meta property="og:image" content="http://inabatorai.com/image/og-facebook.jpg" />
+	<title>日本画家 因幡都頼 公式WEBサイト</title>
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url');?>" type="text/css">
+</head>
+<body>
+<header>
+	<h1 class="title">INABA torai</h1>
+	<nav>
+		<li><a href="#" class="link-works">WORKS</a></li>
+		<li><a href="#" class="link-exhibition">EXHIBITION</a></li>
+		<li><a href="#" class="link-contact">CONTACT</a></li>
+		<li><a href="#" class="link-profile">PROFILE</a></li>
+	</nav>
+<div class="btn-menu">menu</div>
+<div class="sns-container">
+	<a class="btn-sns add-twitter" href="http://twitter.com/share?url=http://inabatorai.com&text=日本画家 因幡都頼 公式WEBサイト" target="_blank"></a>
+	<a class="btn-sns add-facebook" href="http://www.facebook.com/share.php?u=http://inabatorai.com" onclick="window.open(this.href, 'FBwindow', 'width=650, height=450, menubar=no, toolbar=no, scrollbars=yes'); return false;"></a>
+</div>
+</header>
---
+++ index.php
@@ -0,0 +1,271 @@
+<?php get_header();?>
+
+
+
+<div class="main-container">
+	<div class="container topvisual">
+		<div class="topvisual-mobile" style="overflow:scroll">
+			<div id="instafeed" style="z-index: 10;">
+		</div>
+	</div>
+		<div class="info-container">
+				<h2>
+				news
+				</h2>
+				<p class="info-date">
+				2016.5.30
+				</p>
+				<p class="info-text">
+				オフィシャルサイトをオープンしました。
+				</p>
+		</div>
+		<div class="top-bgcolor"></div>
+	</div>
+
+	<div class="container">
+		<div class="container-inner">
+			<a href="img_works_resize/update-wr-2016/img_works_042.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt035">生</div></a>
+			<a href="img_works_resize/update-wr-2016/img_works_041.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt034">河童図</div></a>
+			<a href="img_works_resize/update-wr-2016/img_works_040.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt033">そういうとき</div></a>
+			<a href="img_works_resize/update-wr-2016/img_works_039.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt032">交差点</div></a>
+			<a href="img_works_resize/update-wr-2016/img_works_038.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt031">いつかのその日</div></a>
+			<a href="img_works_resize/update-wr-2016/img_works_037.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt030">信じられるもの</div></a>
+			<a href="img_works_resize/update-wr-2016/img_works_036.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt029">何もない</div></a>
+			<a href="img_works_resize/update-wr-2016/img_works_035.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt028">些細なこと</div></a>
+			<a href="img_works_resize/update-wr-2016/img_works_034.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt027">廻る廻る</div></a>
+			<a href="img_works_resize/update-wr-2016/img_works_033.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt026">僕には分からない</div></a>
+			<a href="img_works_resize/update-wr-2016/img_works_032.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt025">満たされた器</div></a>
+			<a href="img_works_resize/update-wr-2016/img_works_031.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt024">正しい火ト</div></a>
+			<a href="img_works_resize/img_works_036.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt040">本音と建前</div></a>
+			<a href="img_works_resize/img_works_035.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt039">押しても駄目なら引いてみて、引いても駄目なら食べてみる。</div></a>
+			<a href="img_works_resize/img_works_034.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt038">メデューサ</div></a>
+			<a href="img_works_resize/img_works_033.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt037">気づかない。</div></a>
+			<a href="img_works_resize/img_works_032.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt036">もうやだ。</div></a>
+			<a href="img_works_resize/img_works_030.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt023">いつか風は吹きますよ。</div></a>
+			<a href="img_works_resize/img_works_029.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt022">いいもの</div></a>
+			<a href="img_works_resize/img_works_028.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt021">知ってる事、知らない事</div></a>
+			<a href="img_works_resize/img_works_022.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt019">裏側</div></a>
+			<a href="img_works_resize/img_works_021.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt018">小さいおじさん</div></a>
+			<a href="img_works_resize/img_works_020.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt017">小さいおじさん</div></a>
+			<a href="img_works_resize/img_works_019.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt016">振り返っても自分</div></a>
+			<a href="img_works_resize/img_works_018.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt013">原液垂れ流し</div></a>
+			<a href="img_works_resize/img_works_015.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt012">自分が思っているより</div></a>
+			<a href="img_works_resize/img_works_012.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt010">タコ足配線</div></a>
+			<a href="img_works_resize/img_works_011.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt009">河童図</div></a>
+			<a href="img_works_resize/img_works_010.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt008">河童図</div></a>
+			<a href="img_works_resize/img_works_008.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt006">河童図</div></a>
+			<a href="img_works_resize/img_works_009.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt007">日はまた昇る</div></a>
+			<a href="img_works_resize/img_works_007.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt005">後でやる。</div></a>
+			<a href="img_works_resize/img_works_003.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt004">見たい、聞きたい、言いたい</div></a>
+			<a href="img_works_resize/img_works_002.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt003">本音とは程遠い</div></a>
+			<a href="img_works_resize/img_works_001.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt002">共感の行方</div></a>
+			<a href="img_works_resize/img_works_000.jpg" data-lightbox="img-1">
+				<div class="gallery-item wrt001">最早、語るまい</div></a>
+		</div>
+		<div class="scroll-top"></div>
+	</div>
+
+	<div class="container">
+		<div class="container-inner">
+			<a href="img_exhib/update-ex-201605/img_exhib_010.jpg" data-lightbox="group-exh004">
+				<div class="exhibition-item exh004">「そこにない堤防」展 コートギャラリー国立(東京)</div></a>
+			<a href="img_exhib/update-ex-201605/img_exhib_011.jpg" data-lightbox="group-exh004"></a>
+			<a href="img_exhib/update-ex-201605/img_exhib_012.jpg" data-lightbox="group-exh004"></a>
+			<a href="img_exhib/update-ex-201605/img_exhib_013.jpg" data-lightbox="group-exh004"></a>
+			<a href="img_exhib/update-ex-201605/img_exhib_014.jpg" data-lightbox="group-exh004"></a>
+			<a href="img_exhib/img_exhib_001.jpg" data-lightbox="group-exh001">
+				<div class="exhibition-item exh001">因幡都頼個展 g-FAL(東京)</div></a>
+			<a href="img_exhib/img_exhib_002.jpg" data-lightbox="group-exh001"></a>
+			<a href="img_exhib/img_exhib_003.jpg" data-lightbox="group-exh001"></a>
+			<a href="img_exhib/img_exhib_004.jpg" data-lightbox="group-exh002">
+				<div class="exhibition-item exh002">ここだけのはなし(新宿眼科画廊/東京）</div></a>
+			<a href="img_exhib/img_exhib_005.jpg" data-lightbox="group-exh002"></a>
+			<a href="img_exhib/img_exhib_006.jpg" data-lightbox="group-exh002"></a>
+			<a href="img_exhib/img_exhib_007.jpg" data-lightbox="group-exh002"></a>
+			<a href="img_exhib/img_exhib_008.jpg" data-lightbox="group-exh002"></a>
+			<a href="img_exhib/img_exhib_009.jpg" data-lightbox="group-exh003">
+				<div class="exhibition-item exh003">「武蔵野美術大学×ISETAN」新宿伊勢丹(東京)</div></a>
+			<a href="img_exhib/img_exhib_010.jpg" data-lightbox="group-exh003">
+				<div class="exhibition-item exh005">飽和展(ギャラリー創/札幌)</div></a>
+		</div>
+	<div class="scroll-top"></div>
+	</div>
+
+	<div class="container">
+		<div class="text-container">
+				<h2>
+				CONTACT
+				</h2>
+				<p class="info-text">
+				ご連絡、お問い合わせは、下記にご連絡ください。
+				</p>
+				<p class="info-date">
+				E-MAIL
+				</p>
+				<p class="info-text">
+				torai.inaba@gmail.com
+				</p>
+		</div>
+	</div>
+
+	<div class="container">
+		<div class="text-container">
+				<h2>
+				PROFILE
+				</h2>
+				<p class="info-date">
+				INABA torai
+				</p>
+
+<p class="info-text">
+<table>
+<tr><td colspan="2">
+因幡都頼
+</td></tr>
+
+<tr><td colspan="2">
+Inaba Torai
+</td></tr>
+	<a href="img_works_resize/img_works_014.jpg" data-lightbox="img-1">
+		<div class="profile-item wrt011"></div></a>
+<tr><td>
+2012
+</td><td>
+武蔵野美術大学造形学部日本画学科卒業
+</td></tr>
+
+<tr><td>
+1988
+</td><td>
+北海道生まれ
+</td></tr>
+
+<tr><td colspan="2">
+個展
+</td></tr>
+<tr><td>
+2016
+</td><td>
+「そこにない堤防」展 コートギャラリー国立(東京)<br />
+</td></tr>
+<tr><td>
+2015
+</td><td>
+因幡都頼個展 g-FAL(東京)<br />
+因幡都頼個展 gallery創(札幌)<br />
+新さっぽろARTWALL(新さっぽろギャラリー・ARTWALL/札幌)<br />
+</td></tr>
+
+<tr><td>
+2014
+</td><td>
+ここだけのはなし(新宿眼科画廊/東京）<br />
+</td></tr>
+
+<tr><td>
+2013
+</td><td>
+室内あそび(ギャラリー創/札幌)<br />
+</td></tr>
+<tr><td colspan="2">
+グループ展
+</td></tr>
+<tr><td>
+2016
+</td><td>
+「マチネの終わりに作品展」渋谷ヒカリエ8(東京)<br />
+「骨のあるアート」展 新宿伊勢丹(東京)<br />
+</td></tr>
+<tr><td>
+2015
+</td><td>
+「因幡都頼×藤原泰佑」二人展 画廊くにまつ(東京)<br />
+「武蔵野美術大学×ISETAN」新宿伊勢丹(東京)<br />
+「宮本彩香×因幡都頼」二人展 画廊くにまつ(東京)<br />
+</td></tr>
+<tr><td>
+2013
+</td><td>
+PA1NT1NG MASK EX1HIBITION (CASEgallery /東京)<br />
+七彩輪(ギャラリー風/東京)<br />
+</td></tr>
+
+<tr><td>
+2012
+</td><td>
+武蔵野美術大学卒業制作段(武蔵野美術大学/東京)<br />
+東京の美大生(マルヤガーデン/鹿児島)<br />
+飽和展(ギャラリー創/札幌)<br />
+五美術大学連合卒業制作展(新国立美術館/東京)<br />
+武蔵野美術大学日本画学科卒業制作選抜展(コートギャラリー国立/東京)<br />
+湧き筆展(三鷹アトリエ/東京)<br />
+</td></tr>
+
+<tr><td>
+2011
+</td><td>
+日本画孝(武蔵野美術大学/東京)<br />
+the six (ラフォーレ原宿/東京)<br />
+</td></tr>
+
+
+<tr><td colspan="2">
+受賞歴
+</td></tr>
+
+<tr><td>
+2011
+</td><td>
+武蔵野美術大学校友会奨学金奨学生<br />
+</td></tr>
+</table>
+</p>
+
+		</div>
+	</div>
+</div>
+
+<footer>
+C INABA torai 2016
+</footer>
+
+<script type="text/javascript" src="<?php bloginfo('template_url');?>/js/jquery-2.2.3.min.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_url');?>/js/jquery-ui.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_url');?>/js/script.js"></script>
+</body>
+</html>
